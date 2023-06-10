28129751cef8c90563fa83af79aaf69a76d47fe4
Pranjali <rrpranjali@gmail.com>
2023-06-10 16:42:18 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/Search_app" element={<Home />} />
         <Route path="/About" element={<Home />} />
         <Route path="/productShow" element={<ProductShow />} />
